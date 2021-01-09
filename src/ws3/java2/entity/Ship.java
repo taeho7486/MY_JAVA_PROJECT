fabde587c694d159d3f4ae5d2527af2b24988c2d
User <User@DESKTOP-U3RMVVR>
2021-01-09 15:13:22 +0900

    workshop3_10210

--- src/ws3/java2/entity/Ship.java
+++ src/ws3/java2/entity/Ship.java
@@ -1,43 +1,17 @@
 package ws3.java2.entity;
 
-public class Ship {
-	private String modelName;
-	private int maxSpeed;
-	private int numberLimit;
+public class Ship extends Vehicle{
+	
 	private int replacement;
-	private boolean available;
+
 	
 	public Ship(String modelName, int maxSpeed, int numberLimit, int replacement) {
 		
-		this.modelName = modelName;
-		this.maxSpeed = maxSpeed;
-		this.numberLimit = numberLimit;
+		super(modelName, maxSpeed, numberLimit);
 		this.replacement = replacement;
 	}
 
-	public String getModelName() {
-		return modelName;
-	}
 
-	public void setModelName(String modelName) {
-		this.modelName = modelName;
-	}
-
-	public int getMaxSpeed() {
-		return maxSpeed;
-	}
-
-	public void setMaxSpeed(int maxSpeed) {
-		this.maxSpeed = maxSpeed;
-	}
-
-	public int getNumberLimit() {
-		return numberLimit;
-	}
-
-	public void setNumberLimit(int numberLimit) {
-		this.numberLimit = numberLimit;
-	}
 
 	public int getReplacement() {
 		return replacement;
@@ -47,19 +21,8 @@ public class Ship {
 		this.replacement = replacement;
 	}
 
-	public boolean isAvailable() {
-		return available;
-	}
-
-	public void setAvailable(boolean available) {
-		this.available = available;
-	}
 
 	public void displayInfo() {
-		System.out.println("-------------------------------------------");
-		System.out.println(" 모 델 명 : " + this.modelName);
-		System.out.println(" 최고속도 : " + this.maxSpeed + "km/h");
-		System.out.println(" 최대정원 : " + this.numberLimit + "명");
 		System.out.println(" 배 수 량 : " + this.replacement + "톤");
 	}
 }
